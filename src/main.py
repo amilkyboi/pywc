d56e05248e19928a95c7dedebbd4784baddc2024
amilkyboi <30729007+amilkyboi@users.noreply.github.com>
2024-06-10 15:23:10 -0500

    remove unnecessary for loop when counting words

--- src/main.py
+++ src/main.py
@@ -10,8 +10,8 @@ def get_num_bytes(filepath: str) -> int:
     # For reading and writing raw bytes use binary mode and leave encoding unspecified
     # https://docs.python.org/3/library/functions.html#open
     with open(filepath, "rb") as file:
-        # Reads the entire file into a single stream in memory, potentially problematic for very
-        # large files
+        # NOTE: 06/10/24 - Reads the entire file into a single stream in memory, potentially
+        #       problematic for very large files
         byte_data: bytes = file.read()
         num_bytes: int   = len(byte_data)
 
@@ -22,21 +22,19 @@ def get_num_bytes(filepath: str) -> int:
     return num_bytes
 
 def get_num_lines(filepath: str) -> int:
-    with open(filepath, encoding=ENCODING) as file:
+    with open(filepath, 'r', encoding=ENCODING) as file:
         num_lines: int = len(file.readlines())
 
     return num_lines
 
 def get_num_words(filepath: str) -> int:
-    with open(filepath, encoding=ENCODING) as file:
-        num_words: int = 0
+    with open(filepath, 'r', encoding=ENCODING) as file:
+        # Read the entire file into a single string, then split by any whitespace character
+        # (including `\n`, `\r`, `\t`, `\f`, and spaces) and remove empty strings
 
-        for line in file:
-            # Split by any whitespace character, including `\n`, `\r`, `\t`, `\f`, and spaces and
-            # remove empty strings
-            words: list[str] = line.split()
-
-            num_words += len(words)
+        # NOTE: 06/10/24 - Again there are potential memory issues with reading an entire file into
+        #       a single string
+        num_words: int = len(file.read().split())
 
     return num_words
 
