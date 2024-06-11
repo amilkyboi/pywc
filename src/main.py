fef1ae7f0a964f1f7e3b3dbe9ddd84494c812300
amilkyboi <30729007+amilkyboi@users.noreply.github.com>
2024-06-11 10:47:39 -0500

    add support for reading from stdin

--- src/main.py
+++ src/main.py
@@ -9,45 +9,27 @@ from argparse import ArgumentParser, Namespace
 # https://docs.python.org/3/library/locale.html#locale.getencoding
 ENCODING: str = "utf-8"
 
-def get_num_bytes(filepath: Path) -> int:
-    # For reading and writing raw bytes use binary mode and leave encoding unspecified
-    # https://docs.python.org/3/library/functions.html#open
-    with open(filepath, "rb") as file:
-        # NOTE: 06/10/24 - Reads the entire file into a single stream in memory, potentially
-        #       problematic for very large files
-        byte_data: bytes = file.read()
-        num_bytes: int   = len(byte_data)
-
-        # Alternatively, the file can be read in chunks to reduce memory load at the cost of some
-        # performance
-        # https://stackoverflow.com/questions/6787233/python-how-to-read-bytes-from-file-and-save-it
+def get_num_bytes(byte_data: bytes) -> int:
+    num_bytes: int = len(byte_data)
 
     return num_bytes
 
-def get_num_chars(filepath: Path) -> int:
+def get_num_chars(str_data: str) -> int:
     # TODO: 06/10/24 - If the current locale does not support multibyte characters this should match
     #       the number of bytes
-    with open(filepath, "rb") as file:
-        byte_data: bytes = file.read()
-        utf_data:  str   = byte_data.decode(ENCODING)
-        num_chars: int   = len(utf_data)
+    num_chars: int = len(str_data)
 
     return num_chars
 
-def get_num_lines(filepath: Path) -> int:
-    with open(filepath, 'r', encoding=ENCODING) as file:
-        num_lines: int = len(file.readlines())
+def get_num_lines(str_data: str) -> int:
+    num_lines: int = len(str_data.splitlines())
 
     return num_lines
 
-def get_num_words(filepath: Path) -> int:
-    with open(filepath, 'r', encoding=ENCODING) as file:
-        # Read the entire file into a single string, then split by any whitespace character
-        # (including `\n`, `\r`, `\t`, `\f`, and spaces) and remove empty strings
-
-        # NOTE: 06/10/24 - Again there are potential memory issues with reading an entire file into
-        #       a single string
-        num_words: int = len(file.read().split())
+def get_num_words(str_data: str) -> int:
+    # Split by any whitespace character (including `\n`, `\r`, `\t`, `\f`, and spaces) and remove
+    # empty strings
+    num_words: int = len(str_data.split())
 
     return num_words
 
@@ -65,23 +47,35 @@ def main() -> None:
 
     args: Namespace = parser.parse_args()
 
-    # Ensure the filepath exists before converting the string to a Path object
+    str_data:  str
+    byte_data: bytes
+
+    # Read from standard input if no filepath is specified
     if args.filepath is None:
-        # TODO: 06/10/24 - Enable reading from stdin if no filename is provided
-        print("ERROR: no filepath provided")
-        sys.exit(1)
+        # NOTE: 06/11/24 - Reads the entire stdin into a single stream in memory, potentially
+        #       problematic for very large files
+        str_data  = sys.stdin.read()
+        byte_data = str_data.encode(ENCODING)
+    else:
+        filepath: Path = Path(args.filepath)
 
-    filepath: Path = Path(args.filepath)
+        # Ensure the requested filepath exists
+        if not filepath.exists():
+            print(f"ERROR: {filepath} not found")
+            sys.exit(1)
 
-    # Ensure the requested filepath exists
-    if not filepath.exists():
-        print(f"ERROR: {filepath} not found")
-        sys.exit(1)
+        # Ensure the requested filepath is a file (or symlink)
+        if not filepath.is_file():
+            print(f"ERROR: {filepath} is not a file")
+            sys.exit(1)
 
-    # Ensure the requested filepath is a file (or symlink)
-    if not filepath.is_file():
-        print(f"ERROR: {filepath} is not a file")
-        sys.exit(1)
+        # For reading and writing raw bytes use binary mode and leave encoding unspecified
+        # https://docs.python.org/3/library/functions.html#open
+        with open(filepath, "rb") as file:
+            # NOTE: 06/11/24 - Reads the entire file into a single stream in memory, potentially
+            #       problematic for very large files (think about reading in chunks)
+            byte_data = file.read()
+            str_data  = byte_data.decode(ENCODING)
 
     if not any([args.bytes, args.chars, args.lines, args.words]):
         # If no arguments are provided, enable -c, -l and -w by default as in wc
@@ -92,23 +86,26 @@ def main() -> None:
     counts: dict = {"lines": None, "words": None, "chars": None, "bytes": None}
 
     if args.bytes:
-        counts["bytes"] = get_num_bytes(filepath)
+        counts["bytes"] = get_num_bytes(byte_data)
 
     if args.chars:
-        counts["chars"] = get_num_chars(filepath)
+        counts["chars"] = get_num_chars(str_data)
 
     if args.lines:
-        counts["lines"] = get_num_lines(filepath)
+        counts["lines"] = get_num_lines(str_data)
 
     if args.words:
-        counts["words"] = get_num_words(filepath)
+        counts["words"] = get_num_words(str_data)
 
     # The options are always printed in the following order: lines, words, chars, bytes (following
     # convention from wc)
     ordered_counts: list[int] = [counts[key] for key in ["lines", "words", "chars", "bytes"]
                                  if counts[key] is not None]
 
-    print(' '.join(map(str, ordered_counts)), filepath)
+    if args.filepath is None:
+        print(' '.join(map(str, ordered_counts)))
+    else:
+        print(' '.join(map(str, ordered_counts)), args.filepath)
 
 if __name__ == "__main__":
     main()
--- tests/tests.py
+++ tests/tests.py
@@ -7,23 +7,28 @@ import unittest
 from main import get_num_bytes, get_num_lines, get_num_words, get_num_chars
 
 FILEPATH: str = "../data/test.txt"
+ENCODING: str = "utf-8"
+
+with open(FILEPATH, "rb") as file:
+    byte_data = file.read()
+    str_data  = byte_data.decode(ENCODING)
 
 class TestFunctions(unittest.TestCase):
     def test_get_num_bytes(self):
         expected_bytes: int = 342190
-        self.assertEqual(get_num_bytes(FILEPATH), expected_bytes)
+        self.assertEqual(get_num_bytes(byte_data), expected_bytes)
 
     def test_get_num_chars(self):
         expected_chars: int = 339292
-        self.assertEqual(get_num_chars(FILEPATH), expected_chars)
+        self.assertEqual(get_num_chars(str_data), expected_chars)
 
     def test_get_num_lines(self):
         expected_lines: int = 7145
-        self.assertEqual(get_num_lines(FILEPATH), expected_lines)
+        self.assertEqual(get_num_lines(str_data), expected_lines)
 
     def test_get_num_words(self):
         expected_words: int = 58164
-        self.assertEqual(get_num_words(FILEPATH), expected_words)
+        self.assertEqual(get_num_words(str_data), expected_words)
 
 if __name__ == "__main__":
     unittest.main()
